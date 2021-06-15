a10a11874454eb805e4cea8cbbdbc315c9791996
Mark Mavis <mmavis@uw.edu>
2021-06-14 23:24:17 -0700

    add hello_working_copy.cpp to repository

---
+++ C++_Essential_Training/Chap02/hello_working_copy.cpp
@@ -0,0 +1,13 @@
+// hello.cpp by Bill Weinman [bw.org]
+// updated 2020-06-24
+#include <cstdio>
+#define NUMBER 5
+
+int main()
+{
+    puts("Hello, World!");
+    puts("More things to say");
+
+    printf("More things to say agian %d", NUMBER);
+    return 0;
+}
